05e5b9e70c6fbf72bc008bf0edb865b5f5de7009
Brian Howard <bghoward@inmotionsoftware.com>
2015-05-25 10:27:46 -0500

    Updated copyright statement
    Former-commit-id: 941b04ac0d2eb972c7553d97dc9d3c47b0ae44aa [formerly bc8aa3b50496e416a023e574ebb7d5fe3e7032fc]
    Former-commit-id: 2f0ab1d9b02db1df01d155a528fefe241ac83211

--- src/json.hpp
+++ src/json.hpp
@@ -1,13 +1,31 @@
-//
-//  json.hpp
-//  MMapJson
-//
-//  Created by Brian Howard on 5/7/15.
-//  Copyright (c) 2015 InMotion Software, LLC. All rights reserved.
-//
+/*!
+    @file
+    @header json.hpp
+    @author Brian Howard
+    @copyright
+    Copyright (c) 2015 InMotion Software, LLC.
 
-#ifndef MMapJson_json_hpp
-#define MMapJson_json_hpp
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+#ifndef __json_hpp__
+#define __json_hpp__
 
 #if __cplusplus
 
